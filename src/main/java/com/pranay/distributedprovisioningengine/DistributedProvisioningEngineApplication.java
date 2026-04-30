cac9f05281e56381a45af62c20b6fe40fdbca1a0
Pranay <sudanp55@gmail.com>
2026-04-30 11:39:35 +0530

    Initial project setup: Spring Boot + PostgreSQL + layered architecture

---
+++ src/main/java/com/pranay/distributedprovisioningengine/DistributedProvisioningEngineApplication.java
@@ -0,0 +1,13 @@
+package com.pranay.distributedprovisioningengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedProvisioningEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedProvisioningEngineApplication.class, args);
+	}
+
+}
