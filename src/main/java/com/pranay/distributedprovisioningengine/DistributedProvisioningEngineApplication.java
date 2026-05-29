462b4fdd7f591bf23115e45734830ab5bd0dbd34
Pranay <sudanp55@gmail.com>
2026-05-30 00:51:05 +0530

    Fix PostgreSQL timezone issue and finalize monorepo setup

--- src/main/java/com/pranay/distributedprovisioningengine/DistributedProvisioningEngineApplication.java
+++ src/main/java/com/pranay/distributedprovisioningengine/DistributedProvisioningEngineApplication.java
@@ -3,11 +3,21 @@ package com.pranay.distributedprovisioningengine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DistributedProvisioningEngineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DistributedProvisioningEngineApplication.class, args);
-	}
 
+		TimeZone.setDefault(
+				TimeZone.getTimeZone("UTC")
+		);
+
+		SpringApplication.run(
+				DistributedProvisioningEngineApplication.class,
+				args
+		);
+	}
 }
+
